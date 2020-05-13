1e0c77e17a5b3fa877e0a10158e008dec0a38966
Alvarez, Mark <dodgerbluetx@gmail.com>
2020-05-13 04:41:28 +0000

    fixed logging issue in data quality task

--- dags/udac_example_dag.py
+++ dags/udac_example_dag.py
@@ -27,8 +27,8 @@ default_args = {
 dag = DAG('udac_example_dag',
           default_args=default_args,
           description='Load and transform data in Redshift with Airflow',
-          #schedule_interval='0 * * * *',
-          schedule_interval='@monthly',
+          schedule_interval='0 * * * *',
+          #schedule_interval='@monthly',
           template_searchpath='/home/workspace/airflow'
         )
 
